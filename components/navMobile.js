67bcfc5912860f3739fa5bf6ce050274b24a79db
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-14 22:50:53 +1000

    added active tab

--- components/navMobile.js
+++ components/navMobile.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router' 
 
-export default function NavMobile({setMenuStatus, isActive}) {
+export default function NavMobile({setMenuStatus, isActive, menuStatus}) {
 const router = useRouter()
 
 const [dropDown, setDropDown] = useState({services: false, partners: false})
@@ -19,10 +19,13 @@ useEffect(() => {
     }
   }, [setMenuStatus, router])
 
-    return <div className="mobile-menu">
-    <Link href="/" passHref><div className={`mobile-tab ${isActive(router.pathname, '/')}`}>home</div></Link>
-    <Link href="/about" passHref ><div className="mobile-tab">What we do</div></Link>
-     <div onClick={() => setDropDown({services: !dropDown.services, partners: false})} id="services-dropdown" className="mobile-tab-dropdown">services <i className="fas fa-caret-down"></i></div>
+    return <div className={`mobile-menu ${!menuStatus && " close-ani"}`}>
+    <Link href="/" passHref><div className={`mobile-tab ${router.pathname==='/' && 'active'}`}>home</div></Link>
+    <Link href="/about" passHref ><div className={`mobile-tab ${isActive(router.pathname, '/about')}`}>What we do</div></Link>
+     <div onClick={() => setDropDown({services: !dropDown.services, partners: false})} className={`mobile-tab-dropdown ${isActive(router.pathname, '/services')}`}>
+          Services 
+          <i className="fas fa-caret-down"></i>
+     </div>
         { dropDown.services &&
         <div id="services-content" className="mobile-other-dropdown center"> 
           <Link href="/services" passHref><div className="mobile-tab-white">all services</div></Link>
@@ -32,7 +35,10 @@ useEffect(() => {
            <Link href="/services/intellectual-property-financing" passHref><div className="mobile-tab-white">Intellectual Property Financing</div></Link>
            <Link href="/services/disclaimer-and-notices" passHref><div className="mobile-tab-white">Disclaimer & Notices</div></Link>
         </div> }
-     <div onClick={() => setDropDown({services: false, partners: !dropDown.partners})} id="partners-dropdown" className="mobile-tab-dropdown">Senior Partners <i className="fas fa-caret-down"></i></div>
+     <div onClick={() => setDropDown({services: false, partners: !dropDown.partners})} className={`mobile-tab-dropdown ${isActive(router.pathname, '/senior-partners')}`}>
+           Senior Partners 
+           <i className="fas fa-caret-down"/>
+      </div>
       {dropDown.partners && 
       <div id="partners-content" className="mobile-other-dropdown center"> 
         <Link href="/senior-partners" passHref><div className="mobile-tab-white">All partners</div></Link>
@@ -40,6 +46,6 @@ useEffect(() => {
         <Link href="/senior-partners/adam-parr" passHref><div className="mobile-tab-white">Adam Parr</div></Link>
         <Link href="/senior-partners/marc-fuhrman" passHref><div className="mobile-tab-white">Marc Fuhrman</div></Link>
      </div>}
-     <Link href="/contact" passHref><div className="mobile-tab">Contact Us</div></Link>
+    <Link href="/contact" passHref><div className={`mobile-tab ${isActive(router.pathname, '/contact')}`}>Contact Us</div></Link>
   </div>
 }
--- components/navbar.js
+++ components/navbar.js
@@ -2,17 +2,16 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../img/SCS-logo-text.png'
 import logoMini from '../img/logo-no-title.png'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import NavMobile from './navMobile'
 import { useRouter } from 'next/router'
 
 export default function Navbar() { 
     const [menuStatus, setMenuStatus] = useState(false)
-    const [activeCur, setActiveCur] = useState("/")
     const router = useRouter()
 
     const isActive = (route, target) => { 
-          if (route === target) return "active"
+          if (route.startsWith(target)) return "active"
     }
 
     return ( 
@@ -21,14 +20,13 @@ export default function Navbar() {
         <div className="logo-container">
           <Link href="/" passHref><div className="large-logo"><Image src={logo} alt=""/></div></Link>
           <Link href="index" passHref><div className="mini-logo"><Image id="mini-logo" href="index" src={logoMini} alt=""/></div></Link>
-          
         </div>
         <div className="tabs">
             <ul>
                 <li className="right-border"><Link href="/" passHref><div className={router.pathname === '/' && "active"}>Home</div></Link></li>
-                <li className="right-border"><Link href="/about" passHref><div className={router.pathname === '/about' && "active"}>What We Do</div></Link></li>
-                <li className="right-border dropdown-tab"><Link href="/services" passHref><div className={router.pathname.startsWith('/services')}>Services</div></Link>
-                <div className="dropdown-bracket"></div>
+                <li className="right-border"><Link href="/about" passHref><div className={isActive(router.pathname, "/about")}>What We Do</div></Link></li>
+                <li className="right-border dropdown-tab"><Link href="/services" passHref><div className={isActive(router.pathname, "/services")}>Services</div></Link>
+                <div className="dropdown-bracket"/>
                 <div className="dropdown-content">
                   <Link href="/services/structured-finance-strategies">Structured Finance Strategies</Link>
                   <Link href="/services/environmental-finance">Environmental Finance</Link>
@@ -37,7 +35,7 @@ export default function Navbar() {
                   <Link href="/services/disclaimer-and-notices">Disclaimer & Notices</Link>
                 </div>
               </li>
-                <li className="right-border dropdown-tab"><Link href="/senior-partners" passHref><div className={router.pathname.startsWith('/senior-partners')}>Senior Partners</div></Link>
+                <li className="right-border dropdown-tab"><Link href="/senior-partners" passHref><div className={isActive(router.pathname, '/senior-partners')}>Senior Partners</div></Link>
                   <div className="dropdown-bracket"></div>
                   <div className="dropdown-content">
                     <Link href="/senior-partners/paul-edwards">Paul Edwards</Link>
@@ -47,7 +45,7 @@ export default function Navbar() {
                 </li>
                 <li>
                       <Link href="/contact" passHref>
-                          <div className={router.pathname === '/contact' && 'active'}>Contact Us</div>
+                          <div className={isActive(router.pathname, "/contact")}>Contact Us</div>
                       </Link>
                   </li>
             </ul>
@@ -59,7 +57,7 @@ export default function Navbar() {
         </div>
         
     </div>
-    {menuStatus && <NavMobile setMenuStatus={setMenuStatus} isActive={isActive}/>}
+    {menuStatus && <NavMobile setMenuStatus={setMenuStatus} isActive={isActive} menuStatus={menuStatus}/>}
     </div>
     )
      
